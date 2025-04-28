01c42b61e061a5791f3f53878c15b2e5877bd389
rbhullar2000 <robsbhullar@gmail.com>
2025-04-27 23:34:03 -0700

    Add files via upload

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,33 +12,40 @@ export default function Navbar() {
   return (
     <nav className="bg-[#0a0e2c] p-4">
       <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <Link href="/">
-            <img src="/logo.png" alt="Logo" className="h-10 w-auto cursor-pointer" />
-          </Link>
-        </div>
+        {/* Logo */}
+        <Link href="/">
+          <img src="/logo.png" alt="Logo" className="h-10 w-auto cursor-pointer" />
+        </Link>
 
-        <ul className="hidden md:flex space-x-6 text-white text-lg">
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-8 text-white text-lg">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/calculator">Calculator</Link></li>
-          <li><Link href="/pre-approval">Pre-Approval</Link></li> {/* 👈 EXACTLY THIS */}
+          <li><Link href="/pre-approval">Get Pre-Approved</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
 
-        <div className="text-white text-3xl md:hidden cursor-pointer" onClick={toggleMenu}>
+        {/* Mobile Hamburger Button */}
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-white text-3xl focus:outline-none"
+        >
           ☰
-        </div>
+        </button>
       </div>
 
+      {/* Mobile Dropdown Menu */}
       {isMenuOpen && (
-        <ul className="flex flex-col mt-4 space-y-4 text-white text-lg md:hidden">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/calculator">Calculator</Link></li>
-          <li><Link href="/pre-approval">Pre-Approval</Link></li> {/* 👈 EXACTLY THIS */}
-          <li><Link href="/contact">Contact</Link></li>
-        </ul>
+        <div className="md:hidden mt-4 bg-[#0a0e2c] rounded-lg shadow-lg">
+          <ul className="flex flex-col space-y-4 text-white text-center py-4">
+            <li><Link href="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+            <li><Link href="/about" onClick={() => setIsMenuOpen(false)}>About</Link></li>
+            <li><Link href="/calculator" onClick={() => setIsMenuOpen(false)}>Calculator</Link></li>
+            <li><Link href="/pre-approval" onClick={() => setIsMenuOpen(false)}>Get Pre-Approved</Link></li>
+            <li><Link href="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link></li>
+          </ul>
+        </div>
       )}
     </nav>
   );
