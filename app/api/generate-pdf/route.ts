59db3920640121ba7ae5e2dbfe049785a7a2db8b
rbhullar2000 <robsbhullar@gmail.com>
2025-05-08 01:05:27 -0700

    Update route.ts

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -7,7 +7,7 @@ import nodemailer from 'nodemailer';
 import type { NextRequest } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const data: { [key: string]: any } = await req.json();
+  const data: Record<string, string> = await req.json();
   const filePath = path.join(process.cwd(), 'public', 'Credit Application CapRock Capital Group.pdf');
   const pdfBytes = fs.readFileSync(filePath);
   const pdfDoc = await PDFDocument.load(pdfBytes);
