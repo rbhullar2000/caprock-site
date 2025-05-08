0c00ac78663e7cbf271152cb9d96938fb2f73938
rbhullar2000 <robsbhullar@gmail.com>
2025-05-08 09:37:26 -0700

    Update page.tsx

--- app/full-application/continue/page.tsx
+++ app/full-application/continue/page.tsx
@@ -9,7 +9,7 @@ import {
   AccordionItem,
   AccordionTrigger,
   AccordionContent,
-} from "../components/ui/accordion";
+} } from "../../components/ui/accordion";
 
 export default function FullApplicationPage() {
   const searchParams = useSearchParams();
