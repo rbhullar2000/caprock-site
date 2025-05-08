f607546449a3f651764158881bf23eb7276ccb98
rbhullar2000 <robsbhullar@gmail.com>
2025-05-08 12:39:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,1 @@
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-center">
-      <h1 className="text-4xl font-bold text-red-600">🚀 Caprock Homepage is LIVE 🚀</h1>
-    </div>
-  );
-}
+
